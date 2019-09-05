90af264c923ef6bdb842541ad98ef71e40ce5578
Holger Stitz <holger.stitz@datavisyn.io>
2019-09-05 15:03:06 +0200

    Fix tests

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,8 +1,16 @@
 /// <reference types="jasmine" />
-import {trackApp} from '../src/index';
+import {trackProvenance, trackLogin, trackLogout} from '../src/index';
 
 describe('index', () => {
-  it('trackApp() exists', () => {
-    expect(typeof trackApp).toBe('function');
+  it('trackLogin() exists', () => {
+    expect(typeof trackLogin).toBe('function');
+  });
+
+  it('trackLogout() exists', () => {
+    expect(typeof trackLogout).toBe('function');
+  });
+
+  it('trackProvenance() exists', () => {
+    expect(typeof trackProvenance).toBe('function');
   });
 });
