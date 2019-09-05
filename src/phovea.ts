06630627ecff2933d062b0dba5fb0a70002a3983
Holger Stitz <holger.stitz@datavisyn.io>
2019-09-05 12:42:16 +0200

    Rename extension points

--- src/phovea.ts
+++ src/phovea.ts
@@ -4,20 +4,20 @@
  * Licensed under the new BSD license, available at http://caleydo.org/license
  **************************************************************************** */
 import {IRegistry} from 'phovea_core/src/plugin';
-import {PHOVEA_LOGIN, PHOVEA_LOGOUT} from 'phovea_core/src/extensions';
-import {PHOVEA_PROVENANCE_GRAPH} from 'phovea_clue/src/extensions';
+import {EP_PHOVEA_CORE_LOGIN, EP_PHOVEA_CORE_LOGOUT} from 'phovea_core/src/extensions';
+import {EP_PHOVEA_CLUE_PROVENANCE_GRAPH} from 'phovea_clue/src/extensions';
 
 export default function (registry: IRegistry) {
 
-  registry.push(PHOVEA_LOGIN, 'matomoLogin', () => System.import('./matomo'), {
+  registry.push(EP_PHOVEA_CORE_LOGIN, 'matomoLogin', () => System.import('./matomo'), {
     factory: 'trackLogin'
   });
 
-  registry.push(PHOVEA_LOGOUT, 'matomoLogout', () => System.import('./matomo'), {
+  registry.push(EP_PHOVEA_CORE_LOGOUT, 'matomoLogout', () => System.import('./matomo'), {
     factory: 'trackLogout'
   });
 
-  registry.push(PHOVEA_PROVENANCE_GRAPH, 'matomoAnalytics', () => System.import('./matomo'), {
+  registry.push(EP_PHOVEA_CLUE_PROVENANCE_GRAPH, 'matomoAnalytics', () => System.import('./matomo'), {
     factory: 'trackProvenance'
   });
 
